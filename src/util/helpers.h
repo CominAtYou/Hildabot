77df72b75eb313deb149bceef44403bb00fbb695
William M. <cominatyou@cominatyou.com>
2025-04-27 01:27:48 -0500

    Add function docs

--- src/util/helpers.h
+++ src/util/helpers.h
@@ -2,8 +2,16 @@
 #include <cstdint>
 
 namespace util {
+    /// @brief Get the number of seconds since the Unix epoch (1970-01-01).
+    /// @return The number of seconds since the Unix epoch.
     int64_t seconds_since_epoch();
+    /// @brief Get the number of seconds that represent midnight today in the local timezone.
+    /// @return The number of seconds since the Unix epoch that represents midnight today in the local timezone.
     int64_t midnight_today_seconds();
+    /// @brief Get the number of seconds that represent midnight tomorrow in the local timezone.
+    /// @return The number of seconds since the Unix epoch that represents midnight tomorrow in the local timezone.
     int64_t midnight_tomorrow_seconds();
+    /// @brief Get the number of seconds that represent midnight in a week from now in the local timezone.
+    /// @return The number of seconds since the Unix epoch that represents midnight in a week from now in the local timezone.
     int64_t midnight_seconds_in_a_week();
 }
--- src/xp/xp_system.h
+++ src/xp/xp_system.h
@@ -2,6 +2,11 @@
 #include <dpp/dpp.h>
 
 namespace xp {
+    /// @brief Give XP to a user for sending a message.
+    /// @param event The message create event containing the user and message information.
     dpp::task<void> give_xp_for_message(const dpp::message_create_t& event);
+    /// @brief Check if a user has leveled up after an action.
+    /// @param before_action_level The user's level before the action that may have caused a level up.
+    /// @param event The message create event containing the user and message information.
     dpp::task<void> check_for_level_up(const int before_action_level, const dpp::message_create_t& event);
 }
