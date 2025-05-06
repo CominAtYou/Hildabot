b7ccd20e5a3906bc56a8a4ea94574ad90f841569
William M. <cominatyou@cominatyou.com>
2025-05-06 02:10:07 -0500

    Move allowed channels to implementation file

--- src/commands/submit.cc
+++ src/commands/submit.cc
@@ -15,6 +15,18 @@
 
 namespace commands {
     namespace submit {
+        static constexpr uint64_t allowedChannels[] = {
+            492578733442465804ULL,
+            492579714674720778ULL,
+            492580873628286976ULL,
+            492580926111481859ULL,
+            492885164993675264ULL,
+            565327145786802176ULL,
+            1139510301390282802ULL,
+            1139510635223322674ULL,
+            1366529081318838404ULL /* testing channel ID */
+        };
+
         dpp::task<void> execute(const dpp::message_create_t& event, const std::vector<std::string>& args) {
             if (std::find(std::begin(allowedChannels), std::end(allowedChannels), event.msg.channel_id) == std::end(allowedChannels)) {
                 co_return;
