dd5a20466e5a2a15f637e88b21602f2167d988a9
William M. <cominatyou@cominatyou.com>
2025-05-11 11:12:12 -0500

    Code cleanup

--- src/util/helpers.h
+++ src/util/helpers.h
@@ -6,19 +6,24 @@ namespace util {
     /// @brief Get the number of seconds since the Unix epoch (1970-01-01).
     /// @return The number of seconds since the Unix epoch.
     int64_t seconds_since_epoch();
+
     /// @brief Get the number of seconds that represent midnight today in the local timezone.
     /// @return The number of seconds since the Unix epoch that represents midnight today in the local timezone.
     int64_t midnight_today_seconds();
+
     /// @brief Get the number of seconds that represent midnight tomorrow in the local timezone.
     /// @return The number of seconds since the Unix epoch that represents midnight tomorrow in the local timezone.
     int64_t midnight_tomorrow_seconds();
+
     /// @brief Get the number of seconds that represent midnight in a given number of days from now in the local timezone.
     /// @param days The number of days from now.
     /// @return The number of seconds since the Unix epoch that represents midnight in the given number of days from now in the local timezone.
     int64_t midnight_seconds_in_days(const int days);
+
     /// @brief Get the number of seconds that represent midnight in a week from now in the local timezone.
     /// @return The number of seconds since the Unix epoch that represents midnight in a week from now in the local timezone.
     int64_t midnight_seconds_in_a_week();
+
     /// @brief Format a number with commas as thousands separators.
     /// @tparam T The type of the number to format.
     /// @param value The number to format.
