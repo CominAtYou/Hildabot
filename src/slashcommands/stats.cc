9700432afae0f5e96f05f051057b7635edbd3336
William M. <cominatyou@cominatyou.com>
2025-05-18 01:20:05 -0500

    Use only the guild_member object for stats

--- src/slashcommands/stats.cc
+++ src/slashcommands/stats.cc
@@ -11,14 +11,12 @@ namespace slash_commands {
         dpp::task<void> execute(const dpp::slashcommand_t& event) {
             dpp::command_interaction command = event.command.get_command_interaction();
 
-            dpp::user user = event.command.usr;
             dpp::guild_member guild_member = event.command.member;
             if (!command.options.empty()) {
-                user = event.command.get_resolved_user(command.get_value<dpp::snowflake>(0));
                 guild_member = event.command.get_resolved_member(command.get_value<dpp::snowflake>(0));
             }
 
-            UserEntry user_entry(user);
+            UserEntry user_entry(guild_member.user_id);
 
             const int current_level = user_entry.get_level();
             const int current_streak = user_entry.get_streak();
@@ -40,8 +38,8 @@ namespace slash_commands {
             const std::string current_rank_name = rankutil::rank_from_level(current_level).name;
 
             dpp::embed embed = dpp::embed{}
-                .set_title(guild_member.get_nickname().empty() ? user.global_name : guild_member.get_nickname())
-                .set_thumbnail(user.get_avatar_url(1024U, dpp::image_type::i_png, false))
+                .set_title(guild_member.get_nickname().empty() ? guild_member.get_user()->global_name : guild_member.get_nickname())
+                .set_thumbnail(guild_member.get_user()->get_avatar_url(1024U, dpp::image_type::i_png, false))
                 .set_color(HILDA_BLUE)
                 .set_description(std::format("Level {} • {}", current_level, current_rank_name))
                 .add_field("Progress", progress_bar.str())
@@ -50,7 +48,7 @@ namespace slash_commands {
                 .add_field("High Score", util::format_with_commas(high_score), true)
                 .add_field("Kudos", std::format("<:HildaStar:539313415425097728> **Given:** {} | <:HildaStar:539313415425097728> **Received:** {}", util::format_with_commas(user_entry.get_kudos_given()), util::format_with_commas(user_entry.get_kudos_received())))
                 .add_field("Stats", std::format("**Submits:** {} | **Tokens:** {}", util::format_with_commas(user_entry.get_times_submitted()), util::format_with_commas(user_entry.get_tokens())))
-                .add_field("Submit Status", user_entry.has_submitted_today() ? (event.command.usr.id == user.id ? ":white_check_mark: You have submitted today!" : ":white_check_mark: Submitted today!") : event.command.usr.id == user.id ? "You have not submitted today." : "Nothing yet today!");
+                .add_field("Submit Status", user_entry.has_submitted_today() ? (event.command.usr.id == guild_member.user_id ? ":white_check_mark: You have submitted today!" : ":white_check_mark: Submitted today!") : event.command.usr.id == guild_member.user_id ? "You have not submitted today." : "Nothing yet today!");
 
             if (user_entry.get_streak_expiry().has_value()) {
                 embed.add_field("Streak Expiry", std::format("<t:{}>", user_entry.get_streak_expiry().value()));
