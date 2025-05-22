0457c28df4648dd819d01935157175fb5680c28a
William M. <cominatyou@cominatyou.com>
2025-05-22 17:55:02 -0500

    Add debug database

--- src/db/mongo_database.cc
+++ src/db/mongo_database.cc
@@ -8,7 +8,11 @@ namespace MongoDatabase {
     mongocxx::database& get_database() {
         static mongocxx::instance driver_instance{};
         static mongocxx::client client(mongocxx::uri(MONGO_URI));
+        #ifndef DEBUG
         static mongocxx::database db = client["hildabot"];
+        #else
+        static mongocxx::database db = client["hildabot_test"];
+        #endif
 
         return db;
     }
