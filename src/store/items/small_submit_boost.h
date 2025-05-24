a91cf4c886bcf74ed3aad7b49f36caae78df1ed2
William M. <cominatyou@cominatyou.com>
2025-05-23 22:12:57 -0500

    Store price discounts

--- src/store/items/small_submit_boost.h
+++ src/store/items/small_submit_boost.h
@@ -4,6 +4,6 @@
 
 class small_submit_boost : public store_item {
     public:
-        small_submit_boost() : store_item("3 Small Submission Boosts", "Increase the XP you receive for your next 3 submissions by 10%.", "This item can only be purchased once every seven days.", "smallsubmissionboost", 500, purchase_limit::WEEKLY) {}
+        small_submit_boost() : store_item("3 Small Submission Boosts", "Increase the XP you receive for your next 3 submissions by 10%.", "This item can only be purchased once every seven days.", "smallsubmissionboost", 350, purchase_limit::WEEKLY) {}
         dpp::task<bool> give_item(const dpp::button_click_t& event) const override;
 };
--- src/store/items/small_xp_increase.h
+++ src/store/items/small_xp_increase.h
@@ -4,6 +4,6 @@
 
 class small_xp_increase : public store_item {
     public:
-        small_xp_increase() : store_item("150 XP", "Instantly get an extra 150 XP!", "", "smallxp", 450, purchase_limit::UNLIMITED) {}
+        small_xp_increase() : store_item("150 XP", "Instantly get an extra 150 XP!", "", "smallxp", 250, purchase_limit::UNLIMITED) {}
         dpp::task<bool> give_item(const dpp::button_click_t& event) const override;
 };
