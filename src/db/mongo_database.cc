e53eb290f8782ea5f4484cdb9bf7538418e5ae77
William M. <cominatyou@cominatyou.com>
2025-05-30 00:05:47 -0500

    Move mongo variables outside of function

--- src/db/mongo_database.cc
+++ src/db/mongo_database.cc
@@ -4,16 +4,17 @@
 #include <mongocxx/instance.hpp>
 #include <mongocxx/uri.hpp>
 
+const mongocxx::instance driver_instance{};
+const mongocxx::client client(mongocxx::uri(MONGO_URI));
+
+#ifndef DEBUG
+mongocxx::database db = client["hildabot"];
+#else
+mongocxx::database db = client["hildabot_test"];
+#endif
+
 namespace MongoDatabase {
     mongocxx::database& get_database() {
-        static mongocxx::instance driver_instance{};
-        static mongocxx::client client(mongocxx::uri(MONGO_URI));
-        #ifndef DEBUG
-        static mongocxx::database db = client["hildabot"];
-        #else
-        static mongocxx::database db = client["hildabot_test"];
-        #endif
-
         return db;
     }
 }
